cec1c7220329f5ece8f985990b226a895ff93034
Anand Jamani <ajamani@elluminatiinc.com>
2026-03-25 16:59:37 +0530

    new update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stickman.elemental',
+  appName: 'Stickman Elemental',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
